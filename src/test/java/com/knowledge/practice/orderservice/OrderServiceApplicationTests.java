faed7458fb63dfaf407a004148972e2ecc1821c3
yashaswi <yashika.ait@gmail.com>
2026-03-01 19:05:41 +0530

    initial Spring Boot Project

---
+++ src/test/java/com/knowledge/practice/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowledge.practice.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
